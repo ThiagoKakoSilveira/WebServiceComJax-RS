6f9697e497b5ad0805265a47ea044762d42086ad
Thiago Silveira <kako_blackbelt@hotmail.com>
2016-11-23 18:36:59 -0200

    Segue inclusão de um banco de Dados com pattern ConnectionFactory com driver do postgres 9.6 e toda a conecção feita com Postgress

---
+++ loja/src/main/java/br/com/alura/loja/daoBD/CarrinhoDaoBD.java
@@ -0,0 +1,39 @@
+package br.com.alura.loja.daoBD;
+
+import java.util.List;
+
+import br.com.alura.loja.modelo.Carrinho;
+
+public class CarrinhoDaoBD implements CarrinhoDaoInterface{
+
+	@Override
+	public void inserir(Carrinho car) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(Carrinho car) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Carrinho> listarCarrinhos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Carrinho procurarPorCodigo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ loja/src/main/java/br/com/alura/loja/daoBD/CarrinhoDaoInterface.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja.daoBD;
+
+import java.util.List;
+
+import br.com.alura.loja.modelo.Carrinho;
+
+public interface CarrinhoDaoInterface {
+	
+	public void inserir(Carrinho car);
+	
+	public void deletar(Carrinho car);
+	
+	public void atualizar();
+	
+	public List<Carrinho> listarCarrinhos();
+	
+	public Carrinho procurarPorCodigo();
+
+}
